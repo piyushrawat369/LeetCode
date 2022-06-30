8688c0ff64bc92ac8a15bf947591fab82b414585
piyushrawat369 <78429520+piyushrawat369@users.noreply.github.com>
2022-06-30 17:56:58 +0530

    Create code.java

---
+++ Daily Challenge/Minimum Moves to Equal Array Elements II/code.java
@@ -0,0 +1,13 @@
+public int minMoves2(int[] nums) {
+        int n = nums.length;
+        
+        Arrays.sort(nums);
+        
+        int median = (n % 2 == 0)? (nums[n/2] + nums[n/2 -1])/2 : nums[n/2];
+        int steps = 0;
+        
+        for(int num: nums){
+            steps += Math.abs(num - median);
+        }
+        return steps;
+    }
