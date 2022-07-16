829780db718c9ab6d2b73654e8ef59babec32c34
piyushrawat369 <78429520+piyushrawat369@users.noreply.github.com>
2022-07-16 22:27:32 +0530

    Create code.cpp

---
+++ Data Structure/Remove Linked List Elements/code.cpp
@@ -0,0 +1,17 @@
+// Time Complexity: O(N), where N = length of the list. We visit each element only once in the worst case.
+// Space Complexity: O(1)
+
+
+ListNode* removeElements(ListNode* head, int v) {
+        if(head == NULL) {
+            return head;
+        }
+        if(head->val == v) {
+            ListNode* temp = head->next;
+            head->next = NULL;
+            delete(head);
+            return removeElements(temp , v);
+        }
+        head->next = removeElements(head->next , v);
+        return head;
+    }
