04baaa82c5551ec3d9ba5cb229882a2bf9058de7
piyushrawat369 <78429520+piyushrawat369@users.noreply.github.com>
2022-07-20 15:26:16 +0530

    Create code.cpp

---
+++ Data Structure/Symmetric Tree/code.cpp
@@ -0,0 +1,16 @@
+// Time Complexity: O(N)
+// Space Complexity: O(N)
+
+
+class Solution {
+public:
+    bool check(TreeNode* root1, TreeNode* root2){
+        if(root1 == nullptr || root2 == nullptr)
+            return root1 == root2;
+        return root1->val == root2->val and check(root1->left, root2->right) and check(root1->right, root2->left);
+    }
+    
+    bool isSymmetric(TreeNode* root) {
+        return check(root, root);
+    }
+};
