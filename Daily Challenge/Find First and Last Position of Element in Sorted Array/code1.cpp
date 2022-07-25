a920c0d3f21c53397d6162e3a8ad3a033e934849
piyushrawat369 <78429520+piyushrawat369@users.noreply.github.com>
2022-07-25 06:05:16 +0530

    Create code1.cpp

---
+++ Daily Challenge/Find First and Last Position of Element in Sorted Array/code1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        pair<vector<int>::iterator, vector<int>::iterator> range;
+        
+        range = equal_range(nums.begin(), nums.end(), target);
+        int tleft = distance(nums.begin(), range.first);
+        
+        if(tleft == nums.size() || nums[tleft] != target) return {-1, -1};
+        
+        return {tleft, (int)distance(nums.begin(), range.second) - 1};
+    }
+};
