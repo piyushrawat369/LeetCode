c3bae7a3eff16c6d1eaa123787626cea5b0201b7
piyushrawat369 <78429520+piyushrawat369@users.noreply.github.com>
2023-01-14 03:49:08 +0530

    Create code.cpp

---
+++ Practice/Stack/Largest Rectangle in Histogram/code.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        int ans = 0;
+
+        for(int i=0; i<n; i++){
+            if(i+1 < n && heights[i+1] >= heights[i]){
+                continue;
+            }
+            int minH = INT_MAX;
+
+            for(int j=i; j>=0; j--){
+                minH = min(minH, heights[j]);
+                ans = max(ans, minH * (i-j+1));
+            }
+        }
+        
+        return ans;
+    }
+};
