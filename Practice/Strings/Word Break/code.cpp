e240e2ebf0c0c5ddec3eb45e6858c5789bb3a560
piyushrawat369 <78429520+piyushrawat369@users.noreply.github.com>
2023-04-01 15:03:56 +0530

    Create code.cpp

---
+++ Practice/Strings/Word Break/code.cpp
@@ -0,0 +1,27 @@
+//Approach-1: Memoization DP
+
+class Solution {
+    unordered_map<int,bool> mem;
+    unordered_set<string> myset;
+    
+    bool partition(string s,int pos){
+        if(pos==s.size())   return true;
+        if(mem.count(pos))  return mem[pos];
+
+        string t = "";
+        for(int i=pos; i<s.size(); ++i)
+            if(myset.count(s.substr(pos, i-pos+1)) and partition(s, i+1))
+                return mem[pos] = true;
+
+        return mem[pos] = false;
+    }
+
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        for(auto str: wordDict){
+            myset.insert(str);
+        }
+
+        return partition(s, 0);
+    }
+};
