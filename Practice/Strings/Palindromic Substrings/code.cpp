ef9255e6a17ad168b0e79d274fdf117239f360c1
piyushrawat369 <78429520+piyushrawat369@users.noreply.github.com>
2024-02-10 22:35:53 +0530

    Create code.cpp

---
+++ Practice/Strings/Palindromic Substrings/code.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n = s.size();
+        int ans = 0;
+
+        for(int i=0; i<n; i++){
+            int l = i, r = i, cnt = 0;
+
+            while(l >= 0 && r < n && s[l] == s[r]){
+                cnt++;
+                l--;
+                r++;
+            }
+            ans += cnt;
+            l = i, r = i+1, cnt = 0;
+
+            while(l >= 0 && r < n && s[l] == s[r]){
+                cnt++;
+                l--;
+                r++;
+            }
+            ans += cnt;
+        }
+        return ans;
+    }
+};
