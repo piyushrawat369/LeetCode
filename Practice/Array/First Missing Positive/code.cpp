6cf22e92706ae718e404ef0100d3220e3d2a41ee
piyushrawat369 <78429520+piyushrawat369@users.noreply.github.com>
2024-03-26 16:33:09 +0530

    Create code.cpp

---
+++ Practice/Array/First Missing Positive/code.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        unordered_set<int> s(nums.begin(),nums.end());
+        for(int i=1;i<nums.size()+2;i++)
+            if(s.find(i)==s.end())
+                return i;
+        return -1;
+    }
+};
