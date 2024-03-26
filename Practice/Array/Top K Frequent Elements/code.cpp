6f6807d0524da4c7cb05c60f5a60a712738b4017
piyushrawat369 <78429520+piyushrawat369@users.noreply.github.com>
2024-03-26 23:50:46 +0530

    Create code.cpp

---
+++ Practice/Array/Top K Frequent Elements/code.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        priority_queue<pair<int, int>> pq;
+        unordered_map<int, int> mp;
+
+        for(auto it: nums){
+            mp[it]++;
+        }
+        for(auto it: mp){
+            pq.push({it.second, it.first});
+        }
+        
+        vector<int> ans;
+        while(k-- && !pq.empty()){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
+};
